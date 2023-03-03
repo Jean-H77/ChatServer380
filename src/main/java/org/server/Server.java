26f0ac1cdbe5422c6235a390d2c8b916e4b7f7a9
John <Jeanhanna1234567@gmail.com>
2023-03-02 22:41:11 -0800

    Update Server.java

--- src/main/java/org/server/Server.java
+++ src/main/java/org/server/Server.java
@@ -2,7 +2,9 @@ package org.server;
 
 public class Server {
 
+    public static Configuration configuration;
+
     public static void main(String[] args) {
-        Configuration configuration = Configuration.builder().load("configuration.yaml");
+        configuration = Configuration.builder().load("configuration.yaml");
     }
 }
