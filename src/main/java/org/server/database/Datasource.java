0a27fb8f493cedaa7c154e4f80462fe2588519b2
John <Jeanhanna1234567@gmail.com>
2023-03-02 22:47:53 -0800

    HikariCP setup

--- src/main/java/org/server/database/Datasource.java
+++ src/main/java/org/server/database/Datasource.java
@@ -1,5 +1,25 @@
 package org.server.database;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.server.Server;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Datasource {
 
+    public static final HikariDataSource ds = new HikariDataSource();
+
+    static  {
+        ds.setJdbcUrl(Server.configuration.getJdbcUrl());
+        ds.setUsername(Server.configuration.getUsername());
+        ds.setPassword(Server.configuration.getPassword());
+        ds.addDataSourceProperty("cachePrepStmts", "true");
+        ds.addDataSourceProperty("prepStmtCacheSize", "250");
+        ds.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
 }
