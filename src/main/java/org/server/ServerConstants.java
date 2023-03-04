16708cbd80578384acd5b24f48662ad511fdd237
John <Jeanhanna1234567@gmail.com>
2023-03-03 20:13:44 -0800

    create ServerConstants class

---
+++ src/main/java/org/server/ServerConstants.java
@@ -0,0 +1,10 @@
+package org.server;
+
+public class ServerConstants {
+
+    public static final String CONFIGURATION_PATH = "configuration.yaml";
+
+    private ServerConstants() {
+
+    }
+}
--- src/main/java/org/server/ServerLauncher.java
+++ src/main/java/org/server/ServerLauncher.java
@@ -1,16 +1,18 @@
 package org.server;
 
+import static org.server.ServerConstants.CONFIGURATION_PATH;
+
 public class ServerLauncher {
 
-    public static final String CONFIGURATION_PATH = "configuration.yaml";
-
     public static final Configuration configuration = Configuration.load(CONFIGURATION_PATH);
+
+    public final static Server server = new Server(configuration);
 
     public static void main(String[] args) throws InterruptedException {
         launch();
     }
 
     public static void launch() throws InterruptedException {
-        new Server(configuration).run();
+        server.run();
     }
 }
